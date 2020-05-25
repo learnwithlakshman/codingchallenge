d113e780d010b65d738f6e644fb7092febb05511
Lakshman <tanvi.avula@outlook.com>
2020-05-25 06:37:11 +0530

    Coding Challenge Solutions

---
+++ src/main/java/com/lwl/cc/service/CodingQuestionsService.java
@@ -0,0 +1,8 @@
+package com.lwl.cc.service;
+
+public interface CodingQuestionsService{
+		public  boolean  search(int[] arr,int ele);
+		public  int countPrimes(int[] arr);
+		public int biggest(int a, int b, int c);
+		public int biggest(int... a);
+}
---
+++ src/test/java/com/lwl/cc/CodingchallengeApplicationTests.java
@@ -0,0 +1,39 @@
+package com.lwl.cc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.lwl.cc.service.CodingQuestionsService;
+
+@SpringBootTest
+class CodingchallengeApplicationTests {
+	
+	@Autowired
+	private CodingQuestionsService codingService;
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void primeCountTest() {
+		int res = codingService.countPrimes(new int[] {2,3,5,7,9,10,6});
+		assertEquals(4, res);
+	}
+	
+	@Test
+	public void biggestOfThreeNumbers() {
+		int res = codingService.biggest(2,4,9);
+		assertEquals(9, res);
+	}
+	
+	@Test
+	public void biggestOfNnumbers() {
+		int res = codingService.biggest(2,3,5,7,9,10,6);
+		assertEquals(10, res);
+	}
+	
+}
